081730b74f77519cf170db1bfecb048c53fb8487
Laurence Chong <chongl@sheridancollege.ca>
2019-08-07 06:25:58 -0400

    second test

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <Link to="/">Homeeeeeeee</Link>
+    <Link to="/">Home</Link>
 
     <nav>
       <Link to="/helloworld">Hello World</Link>
