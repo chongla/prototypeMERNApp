adbed48c20cf6c40979ec2c0a020aab9df83592c
Laurence Chong <34558191+chongla@users.noreply.github.com>
2019-08-07 07:12:16 -0400

    Update Header.js
    Test ci last time

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <Link to="/">Homeeee</Link>
+    <Link to="/">Home</Link>
 
     <nav>
       <Link to="/helloworld">Hello World</Link>
